20550460c6cada4e4665c529df27eb22b94772b0
CelsoAlmeida <celso.almeida.leite@hotmail.com>
2022-10-14 23:34:28 -0300

    fase1 - update: create logEvents

--- src/server/controllers/home.ts
+++ src/server/controllers/home.ts
@@ -1,20 +1,28 @@
 import {Request, Response} from 'express';
 import { format } from 'date-fns';
 import {v4} from 'uuid'
+import os from 'os'
+import { Log } from '../../modules/logEvents';
+
 export class ControllerHome {
 
     index(req: Request, res: Response)
     { 
-        
+
+        let o = {
+            user: os.userInfo().username,
+            hostname: os.hostname()
+        }
         let date = `${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}`;
-        let uid = v4()
-        console.log(`[log] ${req.method}:index`)
+        let uid = v4();
+
+        Log.saveAs(`log: [ ${o.user}\t${date} ] ${req.method}:index`)
 
         res.json({
             uid:uid,
-            message:"test...",
+            mensagem:'testing ...',
             dtConsulta: date
         });
-
+        
     }
 }
