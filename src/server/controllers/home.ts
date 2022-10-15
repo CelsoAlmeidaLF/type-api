de4c492944fbfdd301731074bbbd2d564e669081
CelsoAlmeida <celso.almeida.leite@hotmail.com>
2022-10-14 21:48:20 -0300

    fase1: update controllerHome

--- src/server/controllers/home.ts
+++ src/server/controllers/home.ts
@@ -1,13 +1,19 @@
 import {Request, Response} from 'express';
-
+import { format } from 'date-fns';
+import {v4} from 'uuid'
 export class ControllerHome {
 
     index(req: Request, res: Response)
     { 
+        
+        let date = `${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}`;
+        let uid = v4()
         console.log(`[log] ${req.method}:index`)
 
         res.json({
-            message:"test..."
+            uid:uid,
+            message:"test...",
+            dtConsulta: date
         });
 
     }
